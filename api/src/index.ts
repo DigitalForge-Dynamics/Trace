f3218c76290d5258bb82cd084ca65870e961c46d
Wes <76014409+Redtigercod4@users.noreply.github.com>
2023-12-04 19:49:47 +0000

    Basic Database Connection using Environment variables

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,9 @@
+import 'dotenv/config';
 import express, { Express } from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app: Express = express();
 const port = process.env.API_PORT;
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
-})
+});
