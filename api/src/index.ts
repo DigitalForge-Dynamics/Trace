31d5c64073269b1fa68a95172d33accd895643f3
Wes <76014409+Redtigercod4@users.noreply.github.com>
2023-12-15 22:06:27 +0000

    Basic configuration for Redis and development docker  instance

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,14 @@
 import "dotenv/config";
 import express, { Express } from "express";
 import { migrator } from "./database/config/databaseClient";
+import { redisClient } from "./database/config/redisClient";
 
 const app: Express = express();
 const port = process.env.API_PORT;
 
-const startupConfiguration = () => {
-  migrator.up();
+const startupConfiguration = async () => {
+  await migrator.up();
+  await redisClient.connect();
 };
 
 app.listen(port, () => {
