f5769b6705b3eb70905f98ae0fa31b797f6778c9
Wes <76014409+Redtigercod4@users.noreply.github.com>
2023-12-21 21:14:25 +0000

    Basic Implementation of the update Asset endpoint

--- api/src/routes/assetsRouter.ts
+++ api/src/routes/assetsRouter.ts
@@ -14,6 +14,7 @@ router.get("/", async (req: Request, res: Response) => {
   if (retrievedAssets.length <= 0) {
     res.status(404).send("No Assets found").end();
     console.log(`No Assets found - Error Code 404`);
+    return;
   }
 
   res.send(retrievedAssets).status(200).end();
@@ -30,6 +31,7 @@ router.get("/:id", async (req: Request<{ id: string }>, res: Response) => {
   if (!retrievedAsset) {
     res.status(404).send("Asset not found").end();
     console.log(`Asset not found - Error Code 404`);
+    return;
   }
   res.send(retrievedAsset).status(200).end();
 });
@@ -38,18 +40,19 @@ router.post("/", async (req: Request, res: Response) => {
   const requestData: AssetAttributes = req.body;
 
   const sanitisedData: AssetAttributes = sanitize<AssetAttributes>(requestData);
-  const isValid: boolean = ajv.validate<AssetAttributes>(
+  const isValidRequest: boolean = ajv.validate<AssetAttributes>(
     "asset",
     sanitisedData
   );
 
-  if (isValid) {
+  if (isValidRequest) {
     const controller = new AssetController();
     const isSuccessfull: Boolean = await controller.create(sanitisedData);
 
     if (!isSuccessfull) {
       res.status(500).send("Unable to create new asset").end();
       console.log(`Unable to create new asset - Error Code 500`);
+      return;
     }
 
     res.status(204).end();
@@ -59,8 +62,46 @@ router.post("/", async (req: Request, res: Response) => {
   }
 });
 
-router.put("/:id", (req: Request, res: Response) => {
-  res.send("NOT IMPLEMENTED: Asset PUT Endpoint");
+router.put("/:id", async (req: Request<{ id: string }>, res: Response) => {
+  const requestData: AssetAttributes = req.body;
+  const requestId: string = req.params.id;
+
+  const controller = new AssetController();
+
+  const isValidAsset: AssetAttributes | null = await controller.findOne(
+    parseInt(requestId)
+  );
+
+  if (!isValidAsset) {
+    res.status(404).send("Unable to find selected Asset to update").end();
+    console.log(`Unable to find selected Asset to update - Error Code 404`);
+    return;
+  }
+
+  const sanitisedData: AssetAttributes = sanitize<AssetAttributes>(requestData);
+  const isValidRequest: boolean = ajv.validate<AssetAttributes>(
+    "asset",
+    sanitisedData
+  );
+
+  if (!isValidRequest) {
+    res.status(400).end();
+    console.log(`Invalid Request - Error Code 400`);
+    return;
+  }
+
+  const isSuccessfull: boolean = await controller.update(
+    parseInt(requestId),
+    requestData
+  );
+
+  if (!isSuccessfull) {
+    res.status(500).send("Unable to update selected asset").end();
+    console.log(`Unable to update selected asset - Error Code 500`);
+    return;
+  }
+
+  res.status(204).end();
 });
 
 router.delete("/:id", (req: Request, res: Response) => {
