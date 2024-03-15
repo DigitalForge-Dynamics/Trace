61dd58ab667c4e555f9f0d5c433dc4e171e0e97d
SolarFactories <SolarFactories@users.noreply.github.com>
2024-03-15 22:43:12 +0000

    Added Settings page template, with placeholder elements.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
 import HomePage from "./pages/dashboard/Dashboard";
+import SettingsPage from "./pages/settings/Settings";
 import PrivateRoutes from "./components/auth/ProtectedRoute";
 import AuthProvider from "./context/auth.context";
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route element={<PrivateRoutes />}>
             <Route path="/" element={<HomePage />} />
+            <Route path="/settings" element={<SettingsPage />} />
           </Route>
         </Routes>
       </AuthProvider>
