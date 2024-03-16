fc714f0323eaf1405f821c97617b82ac66d93ed4
Wes <76014409+Redtigercod4@users.noreply.github.com>
2024-03-16 09:53:34 +0000

    Rework of Umzug to fix migration issue

---
+++ api/src/database/migrations/initial.migration.ts
@@ -0,0 +1,137 @@
+import { Migration } from "../config/databaseClient";
+import { DataTypes, QueryInterface } from "sequelize";
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.createTable("assets", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    assetTag: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    serialNumber: {
+      type: DataTypes.STRING(128),
+      allowNull: true,
+    },
+    modelNumber: {
+      type: DataTypes.STRING(128),
+      allowNull: true,
+    },
+    nextAuditDate: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  });
+
+  await queryInterface.createTable("locations", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    locationName: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    geoLocation: {
+      type: DataTypes.JSON,
+      allowNull: true,
+    },
+    primaryLocation: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  });
+
+  await queryInterface.createTable("settings", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    category: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    categoryData: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+  });
+
+  await queryInterface.createTable("users", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    scopes: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  });
+};
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.dropTable("assets");
+  await queryInterface.dropTable("locations");
+  await queryInterface.dropTable("settings");
+  await queryInterface.dropTable("users");
+};
