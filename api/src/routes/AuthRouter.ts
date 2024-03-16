056d2a08853de6e065cecfd252e69f07a63201e9
SolarFactories <SolarFactories@users.noreply.github.com>
2024-03-16 21:08:35 +0000

    Added logout and register to call authenticateRequest, and register to call authoriseRequest middleware.

--- api/src/routes/AuthRouter.ts
+++ api/src/routes/AuthRouter.ts
@@ -1,13 +1,15 @@
 import express, { Router } from "express";
 import AuthController from "../controllers/AuthController";
+import { authenticateRequest } from "../middlewares/authenticateRequest";
+import { authoriseRequest } from "../middlewares/authoriseRequest";
 
 const router: Router = express.Router();
 const authController = new AuthController();
 
 router.route("/login").post((req, res) => authController.signin(req, res));
 
-router.route("/logout").post();
+router.route("/logout").post(authenticateRequest);
 
-router.route("/register").post((req, res) => authController.signup(req, res));
+router.route("/register").post(authenticateRequest, authoriseRequest, (req, res) => authController.signup(req, res));
 
 export default router;
