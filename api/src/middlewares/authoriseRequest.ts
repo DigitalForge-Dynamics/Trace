aa49c452da4659beba2f85e98f163d6f2378d89e
SolarFactories <SolarFactories@users.noreply.github.com>
2024-03-16 20:53:10 +0000

    Removed redundant check for authorization header within authoriseRequest middleware, as using the user attributes as passed through from authenticateRequest.

--- api/src/middlewares/authoriseRequest.ts
+++ api/src/middlewares/authoriseRequest.ts
@@ -6,14 +6,6 @@ export const authoriseRequest = async (
   res: Response,
   next: NextFunction
 ) => {
-	const authHeader = req.headers.authorization;
-	if (!authHeader) {
-		// Authentication should be being checked within authenticateRequest
-		// Reaching this is indicative, of missing middleware
-		console.log("authoriseRequest middleware called with no authorization header: Missing authenticateRequest middleware.");
-		res.status(500).end();
-		return;
-	}
 	// & unknown to indicate possible presence of other data. Does not affect typing, only documentative.
 	const user: (UserAttributes & unknown) | undefined = res.locals.user;
 	if (!user) {
