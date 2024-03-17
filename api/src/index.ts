7f5ac13fa3fc8dd5a54006ab250b2ef3fa7e1d11
Wes <76014409+Redtigercod4@users.noreply.github.com>
2024-03-17 20:28:40 +0000

    Tell Express to use Error Handler

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,22 +2,24 @@ import "dotenv/config";
 import express, { Express } from "express";
 import { migrator } from "./database/config/databaseClient";
 import { redisClient } from "./database/config/redisClient";
-import cors from 'cors';
+import cors from "cors";
 import helment from "helmet";
 import assetsRouter from "./routes/AssetRouter";
 import locationsRouter from "./routes/LocationRouter";
 import settingsRouter from "./routes/SettingsRouter";
 import authRouter from "./routes/AuthRouter";
 import { rateLimterMiddleware } from "./middlewares/requestRateLimiter";
+import ErrorController from "./controllers/ErrorController";
 
 const app: Express = express();
 const port = process.env.API_PORT;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+app.use(ErrorController.errorHandler);
 app.use(helment());
 app.use(rateLimterMiddleware);
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 // Routes
 app.use("/settings", settingsRouter);
