12987714b7d3cccbf3262053dfe3dbacb80535b4
SolarFactories <SolarFactories@users.noreply.github.com>
2024-03-21 14:58:23 +0000

    Removed RBAC file on API.

--- api/src/utils/RBAC.ts
+++
@@ -1,8 +0,0 @@
-import { Scope } from "./types/attributeTypes";
-
-// TODO: Load dynamically, according to request pathname
-// To not block presently, set to not require any scopes
-export const getRequiredScopes = (requestPath: string): Scope[] | null => {
-  console.log(`getRequiredScopes called with:${requestPath}`);
-  return [];
-};
