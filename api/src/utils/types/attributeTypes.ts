d29c13f8fa0f555ce703a3fc848db7d05047e437
SolarFactories <SolarFactories@users.noreply.github.com>
2024-03-24 23:46:30 +0000

    Added Array case to JsonNetwork type, and replaced terminating never with T for an unhandled case.

--- api/src/utils/types/attributeTypes.ts
+++ api/src/utils/types/attributeTypes.ts
@@ -56,10 +56,14 @@ export interface UserLoginAttributes {
 }
 
 export type JsonNetworkType<T> =
+  // Types with manual reviving
   T extends Date ? string :
+  // Carry these through
   T extends number ? T :
   T extends boolean ? T :
   T extends string ? T :
   T extends JSON ? T :
   T extends object ? { [K in keyof T]?: JsonNetworkType<T[K]>} :
-  never;
+  T extends Array<infer E> ? Array<JsonNetworkType<E>> :
+  // Unknown case
+  T;
