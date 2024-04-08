97198e05b85e6e86dbf1b0cf19643875643b123b
SolarFactories <SolarFactories@users.noreply.github.com>
2024-04-08 22:12:29 +0100

    Added token expiry monitoring with setInterval instead of useEffect, to have it continue to monitor.

--- web/src/context/auth.context.tsx
+++ web/src/context/auth.context.tsx
@@ -6,6 +6,8 @@ import React, {
 import { useNavigate } from "react-router-dom";
 import { AuthContextProps, AuthData, AuthOption } from "../utils/types/authTypes";
 import authStateReducer, { defaultAuthState } from "../hooks/authReducer";
+import { refreshToken } from "../data/api";
+import { getSessionUser } from "../data/storage";
 
 export const AuthContext = createContext<AuthContextProps>({
   authState: defaultAuthState,
@@ -18,8 +20,8 @@ const AuthProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
     authStateReducer,
     defaultAuthState
   );
-
   const navigate = useNavigate();
+  const activeUser = getSessionUser();
 
   const login = useCallback((data: AuthData) => {
     authDispatch({ type: AuthOption.LOGIN, payload: data });
@@ -28,7 +30,29 @@ const AuthProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
 
   const logout = useCallback(() => {
     authDispatch({ type: AuthOption.LOGOUT });
-  }, [authDispatch]);
+    navigate("/");
+  }, [navigate, authDispatch]);
+
+  // Monitor token expiration
+  const tokenMonitor = async () => {
+    if (activeUser === null) {
+      console.log("No active user. Signing out");
+      logout();
+      return;
+    }
+    if (activeUser.refreshExpiry < Date.now() / 1000) {
+      console.log("Refresh expired. Signing out");
+      logout();
+      return;
+    }
+    if (activeUser.expiry < Date.now() / 1000) {
+      console.log("Access expired. Refreshing");
+      const newAuthData: AuthData = await refreshToken(activeUser)
+      return authDispatch({ type: AuthOption.LOGIN, payload: newAuthData });
+    }
+  };
+
+  setInterval(tokenMonitor, 60*1000);
 
   return (
     <AuthContext.Provider value={{ authState, login, logout }}>
