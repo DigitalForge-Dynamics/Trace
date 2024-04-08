abac757264dd044b40ae3aead916fce4815de0bb
SolarFactories <SolarFactories@users.noreply.github.com>
2024-04-08 23:00:27 +0100

    Sequentialised inputs in playwright test due to causing issues.

--- web/playwright/tests/signIn.test.ts
+++ web/playwright/tests/signIn.test.ts
@@ -3,11 +3,9 @@ import { test, expect } from "@playwright/test";
 test.describe("Sign In as regular user", () => {
   test("Signs in with regular user", async ({ page }) => {
     await page.goto("/");
-    await Promise.all([
-      page.getByLabel("Username").type("TEST_USER"),
-      page.getByLabel("Password").type("TEST_USER_PASSWORD"),
-    ]);
-    await page.getByText("Login").click();
-    await expect(page.getByText("Welcome to your Dashboard, TEST_USER!")).toBeVisible();
+    await page.getByLabel("Username").type("TEST_USER");
+    await page.getByLabel("Password").type("TEST_USER_PASSWORD");
+    await page.getByRole("button", { name: "LOGIN" }).click();
+    await expect(page.getByText("Welcome to your Dashboard, TEST_USER")).toBeVisible();
   });
 });
