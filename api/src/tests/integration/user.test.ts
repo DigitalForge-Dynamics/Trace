b66e46f1772cdbe37911d8e5bc1bcdedae88a9e9
SolarFactories <SolarFactories@users.noreply.github.com>
2024-04-11 19:22:59 +0100

    Re-enabled user tests.

--- api/src/tests/integration/user.test.ts
+++ api/src/tests/integration/user.test.ts
@@ -6,7 +6,7 @@ const headers = {
   'Content-Type': 'application/json',
 };
 
-describe.skip("POST /auth", () => {
+describe("POST /auth", () => {
   it.each<[string, string]>([
     ["TEST_ADMIN", "TEST_ADMIN_PASSWORD"],
     ["TEST_USER", "TEST_USER_PASSWORD"],
