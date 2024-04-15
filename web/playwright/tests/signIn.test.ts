6d8877564f6b0fb698367a76a31e96943877ddde
SolarFactories <SolarFactories@users.noreply.github.com>
2024-04-15 19:18:35 +0100

    Reduced API wait time back down to previous 10s. Set playwright to only install chromium browser. Added playwright test for signing in with admin seeded user.

--- web/playwright/tests/signIn.test.ts
+++ web/playwright/tests/signIn.test.ts
@@ -9,3 +9,13 @@ test.describe("Sign In as regular user", () => {
     await expect(page.getByText("Welcome to your Dashboard, TEST_USER")).toBeVisible();
   });
 });
+
+test.describe("Sign In as admin user", () => {
+  test("Signs in with admin user", async ({ page }) => {
+    await page.goto("/");
+    await page.getByLabel("Username").type("TEST_ADMIN");
+    await page.getByLabel("Password").type("TEST_ADMIN_PASSWORD");
+    await page.getByRole("button", { name: "LOGIN" }).click();
+    await expect(page.getByText("Welcome to your Dashboard, TEST_ADMIN")).toBeVisible();
+  });
+});
