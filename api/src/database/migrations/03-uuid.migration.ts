0c72d09c9ce9a1059d58dcd76950785719d27bd0
SolarFactories <SolarFactories@users.noreply.github.com>
2024-05-05 22:32:45 +0100

    Updated uuid migration script to add uuid's to existing fields.

--- api/src/database/migrations/03-uuid.migration.ts
+++ api/src/database/migrations/03-uuid.migration.ts
@@ -1,9 +1,21 @@
 import { DataTypes } from "sequelize";
 import type { Migration } from "../config/databaseClient";
+import User from "../models/user.model";
+import AuthService from "../../services/AuthenticationService";
+import { UUID } from "crypto";
 
 export const up: Migration = async ({ context: queryInterface }) => {
   await queryInterface.addColumn("users", "uuid", DataTypes.STRING);
-  // TODO: Add UUID values to existing entries
+  const users: User[] = await User.findAll({ where: { uuid: null as unknown as UUID } });
+  const authService = new AuthService();
+  const promises: Promise<unknown>[] = [];
+  for (const user of users) {
+    const { username } = user;
+	const uuid = authService.generateUuid(username);
+	const promise = user.update({ uuid });
+	promises.push(promise);
+  }
+  await Promise.all(promises);
 };
 
 export const down: Migration = async ({ context: queryInterface }) => {
