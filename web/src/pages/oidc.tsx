b67d0c16f1a29244a2e11fda49e8bc2c23c6a494
SolarFactories <SolarFactories@users.noreply.github.com>
2024-12-25 18:00:20 +0000

    Added further types based on specifications.

--- web/src/pages/oidc.tsx
+++ web/src/pages/oidc.tsx
@@ -4,7 +4,7 @@ import { setSessionOidcState, getSessionOidcState, removeSessionOidcState } from
 
 const idpUrl = "https://localhost:18000/realms/trace";
 const clientId = "trace-api";
-const clientSecret = ""; // TODO
+const clientSecret = "4bRyTTZEOl1VyqrGQnavVeinxNcCLN1L";
 const redirectUri = "http://localhost:5173/oidc/callback";
 const scope = "openid+profile+email";
 
@@ -27,11 +27,9 @@ export const OidcRequest = () => {
 	if (!wellKnown || !state || !nonce) return "Loading";
 
 	setSessionOidcState(state);
+	const authUrl = `${wellKnown.authorization_endpoint}?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=${state}&nonce=${nonce}`;
 
-	return (
-		<a href={`${wellKnown.authorization_endpoint}?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=${state}&nonce=${nonce}`}
-		>Authorise</a>
-	);
+	return <a href={authUrl}>Authorise via OIDC</a>;
 };
 
 export const OidcCallback = () => {
@@ -69,10 +67,26 @@ export const OidcCallback = () => {
 	return null;
 };
 
+// https://openid.net/specs/openid-connect-discovery-1_0.html
 type WellKnown = {
-	readonly authorization_endpoint: string;
-	readonly token_endpoint: string;
-	readonly jwks_uri: string;
+	readonly issuer: `https://${string}`;
+	readonly authorization_endpoint: `https://${string}`;
+	readonly token_endpoint: `https://${string}`;
+	readonly userinfo_endpoint?: `https://${string}`;
+	readonly jwks_uri: `https://${string}`;
+	readonly registration_endpoint?: `https://${string}`;
+	/** If present, MUST contain "openid" */
+	readonly scopes_supported?: string[];
+	/** MUST contain "code", "id_token", "id_token token" */
+	readonly response_types_supported: string[];
+	/** If omitted, default to ["query", "fragment"] */
+	readonly response_modes_supported?: string[];
+	/** Valid types include "pairwise", "public" */
+	readonly subject_types_supported: string[];
+	/** MUST include RS256. May include "none" */
+	readonly id_token_signing_alg_values_supported: string[];
+	/** Might not be exhaustive */
+	readonly claims_supported: string[];
 };
 
 type TokenResult = {
@@ -86,6 +100,7 @@ type TokenResult = {
 
 const getWellKnown = async (idpUrl: string): Promise<WellKnown> => {
 	const response = await fetch(`${idpUrl}/.well-known/openid-configuration`);
+	// TODO: Validate body
 	const wellKnown: WellKnown = await response.json();
 	return wellKnown;
 };
@@ -109,3 +124,49 @@ const getTokens = async (tokenEndpoint: string, callbackCode: string): Promise<T
 	const tokens: TokenResult = await response.json();
 	return tokens;
 }
+
+const decodeIdToken = (idToken: string): OidcIdToken => {
+	const parts = idToken.split(".");
+	if (parts.length !== 3) throw new Error("Invalid IdToken");
+	// TODO: Signature Verification
+	const bodyEnc: string = parts[1]!;
+	// TODO: Body schema validation
+	const parsed: OidcIdToken = JSON.parse(atob(bodyEnc));
+	return parsed;
+};
+
+// https://openid.net/specs/openid-connect-core-1_0.html#IDToken
+type OidcIdToken = {
+	readonly iss: `https://${string}`;
+	readonly sub: string;
+	/** MUST contain client_id */
+	readonly aud: string | string[];
+	readonly exp: number;
+	readonly iat: number;
+	readonly auth_time?: number;
+	/** If present, MUST verify is the same as generated in initial request. If present in auth request, then IdP MUST include it in Id Token. */
+	readonly nonce?: string;
+	readonly act?: string;
+	readonly amr?: string[];
+	readonly azp?: string;
+};
+
+// https://www.rfc-editor.org/rfc/rfc7517.html
+type JsonWebKey = {
+	readonly kty: string;
+	readonly use?: "sig" | "enc" | string;
+	readonly key_ops?: ("sign" | "verify" | "encrypt" | "decrypt" | "wrapKey" | "unwrapKey" | "deriveKey" | "deriveBits" | string)[];
+	readonly alg?: string;
+	readonly kid?: string;
+	readonly x5u?: string;
+	readonly x5c?: string;
+	readonly x5t?: string;
+	readonly "x5t#S256"?: string;
+};
+
+// https://www.rfc-editor.org/rfc/rfc7517.html
+export type JsonWebKeySet = {
+	readonly keys: JsonWebKey[];
+};
+
+void decodeIdToken;
