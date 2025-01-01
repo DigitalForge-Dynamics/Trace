2f09ba2f2a493c897500d511a467fe68b67e50fa
Redtigercod4 <76014409+Redtigercod4@users.noreply.github.com>
2025-01-01 20:31:38 +0000

    Update DatabaseConfig for missing Entity

--- api/src/database/config/databaseConfig.ts
+++ api/src/database/config/databaseConfig.ts
@@ -10,6 +10,7 @@ import { Setting } from "../entity/setting.entity";
 import { User } from "../entity/user.entity";
 import { UserRole } from "../entity/userRole.entity";
 import { InitalConfiguration1734993303521 } from "../migration/1734993303521-initalConfiguration";
+import { StatusType } from "../entity/statusType.entity";
 
 export class DatabaseConfig implements IDatabaseConfig {
   private static connection: DatabaseConfig;
@@ -19,7 +20,7 @@ export class DatabaseConfig implements IDatabaseConfig {
     this.datasource = new DataSource({
       type: "postgres",
       url: this.generateConnectionString(),
-      entities: [Asset, AuditLog, DeviceType, Location, Role, Setting, User, UserRole],
+      entities: [Asset, AuditLog, DeviceType, Location, Role, Setting, User, UserRole, StatusType],
       migrations: [InitalConfiguration1734993303521],
       cache: {
         duration: 30000
@@ -60,5 +61,12 @@ export class DatabaseConfig implements IDatabaseConfig {
 
   public getRepository<T>(entity: { new(): T; }): ReturnType<DataSource["getRepository"]> {
     return this.datasource.getRepository(entity);
+  }
+
+  // Needs a tidy up
+  public async runMigrations(): Promise<void> {
+    await this.connect();
+
+    this.datasource.runMigrations();
   }
 }
