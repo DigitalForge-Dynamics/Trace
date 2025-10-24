e04f98977bc72a03c451d4e72f6ddccff572957c
Solar <SolarFactories@users.noreply.github.com>
2025-10-24 21:07:09 +0100

    BunJS Workspace (#157)
    * Bump brace-expansion from 1.1.11 to 1.1.12 in /api (#156)
    
    Bumps [brace-expansion](https://github.com/juliangruber/brace-expansion) from 1.1.11 to 1.1.12.
    - [Release notes](https://github.com/juliangruber/brace-expansion/releases)
    - [Commits](https://github.com/juliangruber/brace-expansion/compare/1.1.11...v1.1.12)
    
    ---
    updated-dependencies:
    - dependency-name: brace-expansion
      dependency-version: 1.1.12
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Reset repo to barebones.
    
    * Added basic bunjs root project.
    
    * Add minimal workspace structure.
    
    * Update tabs to spaces, within tsconfig.
    
    * Added initial test file.
    
    * Increase strictness of tsconfig.json.
    
    * Update GitHub workflows.
    
    * Use catalog version of typescript in root package.json.
    
    * Remove api/package-lock.json, as introduced from merge.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- api/src/services/IService.ts
+++
@@ -1,7 +0,0 @@
-import { PaginationResult } from "../utils/Paginator";
-
-export interface IService<TEntity> {
-  findAll(): Promise<TEntity[]>;
-  findById(id: number): Promise<TEntity | null>;
-  findAllPaginated(page: number, pageSize: number): Promise<PaginationResult<TEntity>>;
-}
