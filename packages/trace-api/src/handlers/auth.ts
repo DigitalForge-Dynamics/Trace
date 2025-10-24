9416616bf83c2ac9a174f497841d3b3744bedc4e
SolarFactories <SolarFactories@users.noreply.github.com>
2025-10-24 22:58:03 +0100

    Added try-catch, to send the error.

--- packages/trace-api/src/handlers/auth.ts
+++ packages/trace-api/src/handlers/auth.ts
@@ -5,14 +5,18 @@ const ISSUER = "https://token.actions.githubusercontent.com";
 //const AUDIENCE = "https:///github.com/DigitalDynamics/Trace";
 
 export const authenticateOidc = async (req: Request) => {
-	const header = req.headers.get("authorization");
-	if (!header) return Response.json({ message: "Missing Authorization" }, { status: 401 });
-	if (!header.startsWith(HEADER_PREFIX)) return Response.json({ message: "Unexpected format" }, { status: 401 });
-	const jwt = header.substring(HEADER_PREFIX.length);
-	const jwks = createRemoteJWKSet(new URL(`${ISSUER}/.well-known/jwks`));
-	const { payload } = await jwtVerify(jwt, jwks, {
-		//issuer: ISSUER,
-		//audience: AUDIENCE,
-	});
-	return Response.json({ message: "Authenticated", data: payload }, { status: 200 });
+	try {
+		const header = req.headers.get("authorization");
+		if (!header) return Response.json({ message: "Missing Authorization" }, { status: 401 });
+		if (!header.startsWith(HEADER_PREFIX)) return Response.json({ message: "Unexpected format" }, { status: 401 });
+		const jwt = header.substring(HEADER_PREFIX.length);
+		const jwks = createRemoteJWKSet(new URL(`${ISSUER}/.well-known/jwks`));
+		const { payload } = await jwtVerify(jwt, jwks, {
+			//issuer: ISSUER,
+			//audience: AUDIENCE,
+		});
+		return Response.json({ message: "Authenticated", data: payload }, { status: 200 });
+	} catch (error) {
+		return Response.json({ error }, { status: 500 });
+	}
 };
