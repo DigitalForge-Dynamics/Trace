134271d6b4baa621ff6f8cce166c3dbb86f5ab85
Redtigercod4 <76014409+Redtigercod4@users.noreply.github.com>
2025-10-24 22:32:37 +0100

    Basic Router based off Hono & Verb packages

--- packages/trace-api/src/index.test.ts
+++ packages/trace-api/src/index.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "bun:test";
 import { packageName } from ".";
-import packageJson from "./package.json";
+import packageJson from "../package.json";
 
 describe("trace-api", () => {
   it("Has expected package name", () => {
