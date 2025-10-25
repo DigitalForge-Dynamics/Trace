821976bc12d8e717f17c9088f373840ff3b00bd2
SolarFactories <SolarFactories@users.noreply.github.com>
2025-10-25 16:00:34 +0100

    Apply biome lints.

--- packages/trace-api/index.test.ts
+++ packages/trace-api/index.test.ts
@@ -1,9 +1,9 @@
-import { describe, it, expect } from "bun:test";
+import { describe, expect, it } from "bun:test";
 import { packageName } from ".";
 import packageJson from "./package.json";
 
 describe("trace-api", () => {
-  it("Has expected package name", () => {
-    expect(packageJson.name).toEqual(packageName);
-  });
+	it("Has expected package name", () => {
+		expect(packageJson.name).toEqual(packageName);
+	});
 });
