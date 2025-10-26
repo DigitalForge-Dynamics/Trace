9083e6455d862767639d22191b9c2df6815199bc
SolarFactories <SolarFactories@users.noreply.github.com>
2025-10-26 13:51:49 +0000

    Remove unncessary index.ts within trace-api, changing server.ts back to index.ts.

--- packages/trace-api/src/index.ts
+++ packages/trace-api/src/index.ts
@@ -1,4 +1,21 @@
-import { server } from "./server.ts";
+import { serve } from "bun";
+import { authenticateOidc } from "./handlers/auth.ts";
+import { createRouter } from "./routes/router.ts";
+
+const router = createRouter();
+
+router.get("/health-check", (): Response => Response.json({ health: "OK" }, { status: 200 }));
+router.post("/auth/oidc", ({ req }) => authenticateOidc(req));
+
+const server = (port: number): ReturnType<typeof serve> => {
+  const server = serve({
+    port,
+	hostname: "localhost",
+    fetch: router.fetch,
+  });
+  console.log(`Server running at ${server.url}`);
+  return server;
+};
 
 if (process.env.NODE_ENV !== "test") {
 	server(3000);
