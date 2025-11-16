7ba4f8e46114505b465394ac811b7502f9981186
SolarFactories <SolarFactories@users.noreply.github.com>
2025-11-16 14:46:54 +0000

    [trace-web]: Updated trace-web, to use OIDC Config for IdP's, as obtained from API.

--- packages/trace-api/src/config.ts
+++ packages/trace-api/src/config.ts
@@ -6,6 +6,24 @@ const oidcConfig = [
     subject:
       "^(repo:DigitalForge-Dynamics\/Trace:ref:refs\/heads\/[^/]+)|(repo:DigitalForge-Dynamics\/Trace:pull_request)$",
   },
+  {
+    label: "Keycloak",
+    issuer: Bun.env.KEYCLOAK_ISSUER,
+    audience: Bun.env.KEYCLOAK_AUDIENCE,
+    subject: "^.*$",
+  },
+  {
+    label: "Cloudflare",
+    issuer: Bun.env.CLOUDFLARE_ISSUER,
+    audience: Bun.env.CLOUDFLARE_AUDIENCE,
+    subject: "^.*$",
+  },
+  {
+    label: "GitHub",
+    issuer: "https://github.com/login/oauth",
+    audience: Bun.env.GITHUB_AUDIENCE,
+    subject: "^.*$",
+  },
 ];
 
 export { oidcConfig };
--- packages/trace-web/src/oidc-manager.ts
+++ packages/trace-web/src/oidc-manager.ts
@@ -1,19 +1,21 @@
 import { UserManager } from "oidc-client-ts";
-import { OIDC_CLIENT_ID, OIDC_ISSUER } from "./.config.ts";
+import { NetClient, APIClient } from "trace-sdk";
 
-const metadata = await fetch(`${OIDC_ISSUER}/.well-known/openid-configuration`).then((response) => response.json());
+const API_URL = new URL("http://localhost:3000");
+const netClient = new NetClient(API_URL);
+const apiClient = new APIClient(netClient);
+
+const oidcIdpConfig = await apiClient.getOidcConfig();
+const idp = oidcIdpConfig.config.find((idp) => idp.label === "Keycloak");
+if (!idp) throw new Error("Unable to identify Keycloak IdP. Please configure.");
 
 const userManager = new UserManager({
-  authority: OIDC_ISSUER,
-  client_id: OIDC_CLIENT_ID,
+  authority: idp.issuer.toString(),
+  client_id: idp.audience,
   redirect_uri: "https://localhost:5173/oidc-callback",
   response_type: "code",
   scope: "openid email",
   automaticSilentRenew: false,
-  metadata: {
-    ...metadata,
-    //token_endpoint: "https://localhost:5173/oidc-token",
-  },
 });
 
-export { userManager, metadata };
+export { userManager };
