06b350c468a6fd737e477bced316ca04423770c7
SolarFactories <SolarFactories@users.noreply.github.com>
2025-11-22 18:18:13 +0000

    [trace-api]: Fixed issue of attempting to use empty object for JWKS. Fixed issue of issuer comparison not accounting for trailing / in URL pathname when serialised from URL object. Add error handling, to reduce the number of 500's returned by authenticateOidc.

--- packages/trace-api/src/handlers/auth.ts
+++ packages/trace-api/src/handlers/auth.ts
@@ -1,9 +1,11 @@
-import { createRemoteJWKSet, type JWTPayload, jwtDecrypt, jwtVerify } from "jose";
+import { createRemoteJWKSet, decodeJwt, type JWTPayload, jwtVerify } from "jose";
+import { JWTClaimValidationFailed, JWTExpired } from "jose/errors";
 import type { oidcConfigResponse } from "trace-schemas";
 import type { z } from "zod";
 import { oidcConfig } from "../config.ts";
 
 const authenticateOidc = async (req: Request): Promise<Response> => {
+  // Load Token
   const HeaderPrefix = "Bearer ";
   const header = req.headers.get("authorization");
   if (!header) {
@@ -13,28 +15,50 @@ const authenticateOidc = async (req: Request): Promise<Response> => {
     return Response.json({ message: "Unexpected format" }, { status: 401 });
   }
   const jwt = header.substring(HeaderPrefix.length);
-  const unverified = await jwtDecrypt<JWTPayload & Required<Pick<JWTPayload, "iss">>>(
-    jwt,
-    {},
-    { requiredClaims: ["iss"] },
-  );
-  const requestedIdp = oidcConfig.find((idp) => idp.issuer.toString() === unverified.payload.iss);
+
+  // Select Issuer
+  const unverified = await decodeJwt<Required<Pick<JWTPayload, "isf">>>(jwt);
+  if (!unverified.iss) {
+    return Response.json({ message: "Missing issuer" }, { status: 401 });
+  }
+  if (!URL.canParse(unverified.iss)) {
+    return Response.json({ message: "Invalid issuer" }, { status: 401 });
+  }
+  const requestedIssuer = new URL(unverified.iss);
+  const requestedIdp = oidcConfig.find((idp) => idp.issuer.toString() === requestedIssuer.toString());
   if (requestedIdp === undefined) {
     return Response.json({ message: "Unknown Token issuer" }, { status: 401 });
   }
+
+  // Verify Token against issuer
   const jwks = createRemoteJWKSet(new URL("/.well-known/jwks", requestedIdp.issuer));
-  const { payload } = await jwtVerify<JWTPayload & Required<Pick<JWTPayload, "iss" | "aud" | "sub" | "iat" | "nbf">>>(
-    jwt,
-    jwks,
-    {
-      issuer: requestedIdp.issuer.toString(),
-      audience: requestedIdp.audience,
-      requiredClaims: ["sub", "iat", "nbf"],
-    },
-  );
+  let payload: JWTPayload & Required<Pick<JWTPayload, "iss" | "aud" | "sub" | "iat" | "nbf">>;
+  try {
+    const result = await jwtVerify<JWTPayload & Required<Pick<JWTPayload, "iss" | "aud" | "sub" | "iat" | "nbf">>>(
+      jwt,
+      jwks,
+      {
+        audience: requestedIdp.audience,
+        requiredClaims: ["iss", "sub", "iat", "nbf"],
+      },
+    );
+    payload = result.payload;
+  } catch (err) {
+    if (err instanceof JWTClaimValidationFailed) {
+      return Response.json({ message: "Failed validation" }, { status: 401 });
+    }
+    if (err instanceof JWTExpired) {
+      return Response.json({ message: "Token Expired" }, { status: 401 });
+    }
+    throw err;
+  }
+
+  // Verify user restrictions
   if (!new RegExp(requestedIdp.subject).test(payload.sub)) {
     return Response.json({ message: "Unauthorised", sub: payload.sub }, { status: 403 });
   }
+
+  // Return generated user token
   // TODO: Generate User Token, and return in request.
   return Response.json({ message: "Authenticated", data: payload }, { status: 200 });
 };
