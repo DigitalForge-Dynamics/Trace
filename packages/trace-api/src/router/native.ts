bc69c5dad995b428ba18fbf9f1b4e81477fb2f7e
SolarFactories <SolarFactories@users.noreply.github.com>
2025-11-23 13:11:45 +0000

    [trace-api]: Added JSDoc comments to Router.

--- packages/trace-api/src/router/native.ts
+++ packages/trace-api/src/router/native.ts
@@ -23,6 +23,11 @@ type Layer =
   | { type: "middleware"; middleware: Middleware<Params> }
   | { type: "error"; handler: ErrorHandler };
 
+/**
+ * Custom middleware based routing, allowing for ExpressJS style interface.
+ * Using strictly typed route handlers for type inference from path parameters.
+ * See Unit Tests for examples.
+ */
 class Router<in out TParams extends Params> {
   private readonly layers: Layer[];
 
@@ -30,23 +35,12 @@ class Router<in out TParams extends Params> {
     this.layers = [];
   }
 
-  on<TPath extends `/${string}`>(
-    method: HttpMethod,
-    path: TPath,
-    handler: NativeHandler<TParams & Bun.Serve.ExtractRouteParams<TPath>>,
-  ): this {
-    this.layers.push({
-      type: "route",
-      route: {
-        method,
-        path,
-        // Type cast needed, as the internal implementation details of `layers` is more weakly typed than the strictly typed function parameters.
-        handler: handler as NativeHandler<Params>,
-      },
-    });
-    return this;
-  }
-
+  /**
+   * Define a route, with handler for the GET HTTP Method
+   * @param path - Path on which to define handler.
+   * @param handler - Strictly typed handler, for corresponding path.
+   * @return Router to allow for chaining definitions.
+   */
   get<TPath extends `/${string}`>(
     path: TPath,
     handler: NativeHandler<TParams & Bun.Serve.ExtractRouteParams<TPath>>,
@@ -54,6 +48,12 @@ class Router<in out TParams extends Params> {
     return this.on("GET", path, handler);
   }
 
+  /**
+   * Define a route, with handler for the POST HTTP Method
+   * @param path - Path on which to define handler.
+   * @param handler - Strictly typed handler, for corresponding path.
+   * @return Router to allow for chaining definitions.
+   */
   post<TPath extends `/${string}`>(
     path: TPath,
     handler: NativeHandler<TParams & Bun.Serve.ExtractRouteParams<TPath>>,
@@ -61,6 +61,12 @@ class Router<in out TParams extends Params> {
     return this.on("POST", path, handler);
   }
 
+  /**
+   * Define a route, with handler for the PUT HTTP Method
+   * @param path - Path on which to define handler.
+   * @param handler - Strictly typed handler, for corresponding path.
+   * @return Router to allow for chaining definitions.
+   */
   put<TPath extends `/${string}`>(
     path: TPath,
     handler: NativeHandler<TParams & Bun.Serve.ExtractRouteParams<TPath>>,
@@ -68,6 +74,12 @@ class Router<in out TParams extends Params> {
     return this.on("PUT", path, handler);
   }
 
+  /**
+   * Define a route, with handler for the PATCH HTTP Method
+   * @param path - Path on which to define handler.
+   * @param handler - Strictly typed handler, for corresponding path.
+   * @return Router to allow for chaining definitions.
+   */
   patch<TPath extends `/${string}`>(
     path: TPath,
     handler: NativeHandler<TParams & Bun.Serve.ExtractRouteParams<TPath>>,
@@ -75,6 +87,12 @@ class Router<in out TParams extends Params> {
     return this.on("PATCH", path, handler);
   }
 
+  /**
+   * Define a route, with handler for the DELETE HTTP Method
+   * @param path - Path on which to define handler.
+   * @param handler - Strictly typed handler, for corresponding path.
+   * @return Router to allow for chaining definitions.
+   */
   delete<TPath extends `/${string}`>(
     path: TPath,
     handler: NativeHandler<TParams & Bun.Serve.ExtractRouteParams<TPath>>,
@@ -82,6 +100,11 @@ class Router<in out TParams extends Params> {
     return this.on("DELETE", path, handler);
   }
 
+  /**
+   * Add a layer of middleware to run on all routes.
+   * @param middleware - Middleware function.
+   * @return Router to allow for chaining definitions.
+   */
   middleware(middleware: Middleware<TParams>): this {
     this.layers.push({
       type: "middleware",
@@ -91,6 +114,12 @@ class Router<in out TParams extends Params> {
     return this;
   }
 
+  /**
+   * Add another router to allow for gradual path dispatch.
+   * @param prefix - The path prefix on which to route to router.
+   * @param router - The sub router.
+   * @return Router to allow for chaining definitions.
+   */
   mount<TPath extends `/${string}`>(
     prefix: TPath,
     router: Router<TParams & Bun.Serve.ExtractRouteParams<TPath>>,
@@ -104,6 +133,11 @@ class Router<in out TParams extends Params> {
     return this;
   }
 
+  /**
+   * Define a new error handler for errors thrown during routes.
+   * The handler that is invoked will be the last one defined before the route that throws.
+   * @param handler - The error handler.
+   */
   errorHandler(handler: ErrorHandler): this {
     this.layers.push({
       type: "error",
@@ -112,6 +146,10 @@ class Router<in out TParams extends Params> {
     return this;
   }
 
+  /**
+   * Create a Bun Routes object, from the middleware stack.
+   * @return Bun Routes - The aggregated handlers, accounting for error handling, and middleware stacks.
+   */
   toNative(): Bun.Serve.Routes<unknown, string> & Record<string, Partial<Record<HttpMethod, NativeHandler<Params>>>> {
     const result: Record<string, Partial<Record<HttpMethod, NativeHandler<Params>>>> = {} satisfies Bun.Serve.Routes<
       unknown,
@@ -213,12 +251,35 @@ class Router<in out TParams extends Params> {
     return result;
   }
 
+  /**
+   * The default error handler if one is not defined within the routing tree.
+   * @param req - The Bun server request
+   * @param error - The value that was thrown during running a route handler.
+   * @return Response - The response for Bun Server to return to the client.
+   */
   static defaultErrorHandler(req: BunRequest, error: unknown): Response {
     console.error(`Unexpected error when handling ${req.method} ${req.url}`);
     if (error instanceof Error) {
       console.error(`Further info: ${error.name}, ${error.message}: ${error.stack}`);
     }
     return Response.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+
+  private on<TPath extends `/${string}`>(
+    method: HttpMethod,
+    path: TPath,
+    handler: NativeHandler<TParams & Bun.Serve.ExtractRouteParams<TPath>>,
+  ): this {
+    this.layers.push({
+      type: "route",
+      route: {
+        method,
+        path,
+        // Type cast needed, as the internal implementation details of `layers` is more weakly typed than the strictly typed function parameters.
+        handler: handler as NativeHandler<Params>,
+      },
+    });
+    return this;
   }
 }
 
