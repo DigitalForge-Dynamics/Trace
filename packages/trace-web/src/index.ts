7740a289fedcea517348546d464bbfbe1debf6a5
SolarFactories <SolarFactories@users.noreply.github.com>
2025-11-23 20:07:40 +0000

    [trace-web]: Updated trace-web to use trace-router.

--- packages/trace-web/src/index.ts
+++ packages/trace-web/src/index.ts
@@ -1,4 +1,5 @@
 import { renderToReadableStream } from "react-dom/server";
+import { Router } from "trace-router";
 import { App } from "./App.tsx";
 
 const serveJs = (filename: string) => async (): Promise<Response> => {
@@ -21,30 +22,34 @@ const serveJs = (filename: string) => async (): Promise<Response> => {
   return new Response(text, { headers: { "Content-Type": script.type } });
 };
 
+const router: Router<Record<string, never>> = new Router();
+
+router.get("/oidc-manager.js", serveJs("oidc-manager.ts"));
+router.get("/oidc-callback", async () => {
+  const srcFile = Bun.file("./src/oidc-callback.html");
+  const text = await srcFile.text();
+  return new Response(text, { headers: { "Content-Type": srcFile.type } });
+});
+
+router.get("/favicon.ico", async () => {
+  const assetFile = Bun.file("./assets/favicon-32x32.png");
+  const contents = await assetFile.text();
+  return new Response(contents, { headers: { "Content-Type": assetFile.type } });
+});
+
+router.get("/hydrate.js", serveJs("hydrate.tsx"));
+router.get("/", async () => {
+  const stream = await renderToReadableStream(App(), {
+    bootstrapModules: ["/hydrate.js"],
+  });
+  return new Response(stream, { headers: { "Content-Type": "text/html" } });
+});
+
 Bun.serve({
   port: 5173,
   tls: {
     keyFile: "tls/server_key.pem",
     certFile: "tls/server_cert.pem",
   },
-  routes: {
-    "/oidc-callback": async () => {
-      const srcFile = Bun.file("./src/oidc-callback.html");
-      const text = await srcFile.text();
-      return new Response(text, { headers: { "Content-Type": srcFile.type } });
-    },
-    "/favicon.ico": async () => {
-      const assetFile = Bun.file("./assets/favicon-32x32.png");
-      const contents = await assetFile.text();
-      return new Response(contents, { headers: { "Content-Type": assetFile.type } });
-    },
-    "/oidc-manager.js": serveJs("oidc-manager.ts"),
-    "/hydrate.js": serveJs("hydrate.tsx"),
-    "/": async () => {
-      const stream = await renderToReadableStream(App(), {
-        bootstrapModules: ["/hydrate.js"],
-      });
-      return new Response(stream, { headers: { "Content-Type": "text/html" } });
-    },
-  },
+  routes: router.toNative(),
 });
