b2603a8bc92c5329cc7b96c00f6d3bcb38632199
SolarFactories <SolarFactories@users.noreply.github.com>
2025-11-23 15:40:52 +0000

    [trace-db]: Added preliminary DB tests, and tables.

---
+++ packages/trace-db/src/index.test.ts
@@ -0,0 +1,59 @@
+import { describe, expect, it } from "bun:test";
+import { SQL } from "bun";
+import { Database } from "./index.ts";
+
+const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+
+describe("Database", () => {
+  it("Is able to baseline an SQLITE database", async () => {
+    const sql = new SQL("sqlite://:memory:");
+    const db = new Database(sql);
+    await db.baseline();
+  });
+
+  describe("Users", () => {
+    it("Is able to create a user", async () => {
+      const sql = new SQL("sqlite://:memory:");
+      const db = new Database(sql);
+      await db.baseline();
+      const user = await db.createUser({ username: "test" });
+      expect(user).toStrictEqual({
+        username: "test",
+        uid: expect.stringMatching(uuidRegex),
+      });
+    });
+
+    it("findUser returns null if the user does not exist", async () => {
+      const sql = new SQL("sqlite://:memory:");
+      const db = new Database(sql);
+      await db.baseline();
+      const idp = await db.createIdp({ issuer: new URL("https://localhost:0") });
+      const foundUser = await db.findUser(idp.issuer, "");
+      expect(foundUser).toBeNull();
+    });
+
+    it("Is able to retrieve a created user", async () => {
+      const sql = new SQL("sqlite://:memory:");
+      const db = new Database(sql);
+      await db.baseline();
+      const createdUser = await db.createUser({ username: "test" });
+      const idp = await db.createIdp({ issuer: new URL("https://localhost:0") });
+      await db.linkUser(createdUser.uid, idp.uid, "foo");
+      const foundUser = await db.findUser(idp.issuer, "foo");
+      expect(foundUser).toStrictEqual(createdUser);
+    });
+  });
+
+  describe("Idps", () => {
+    it("Is able to create an IdP", async () => {
+      const sql = new SQL("sqlite://:memory:");
+      const db = new Database(sql);
+      await db.baseline();
+      const idp = await db.createIdp({ issuer: new URL("https://localhost:0") });
+      expect(idp).toStrictEqual({
+        issuer: new URL("https://localhost:0"),
+        uid: expect.stringMatching(uuidRegex),
+      });
+    });
+  });
+});
--- packages/trace-db/src/index.ts
+++ packages/trace-db/src/index.ts
@@ -1,5 +1,96 @@
-import { SQL } from "bun";
+import { randomUUIDv7, type SQL } from "bun";
 
-const db: SQL = new SQL(":memory:");
+type CreateUser = { username: string };
+type User = { uid: string; username: string };
 
-export { db };
+type CreateIdp = { issuer: URL };
+type Idp = { uid: string; issuer: URL };
+
+class Database {
+  private readonly driver: SQL;
+
+  constructor(driver: SQL) {
+    this.driver = driver;
+  }
+
+  async baseline(): Promise<void> {
+    await this.driver`
+		CREATE TABLE users (
+			uid BINARY(16) PRIMARY KEY,
+			username STRING
+		)
+	`;
+    await this.driver`
+		CREATE TABLE idps (
+			uid BINARY(16) PRIMARY KEY,
+			issuer URL
+		)
+	`;
+    await this.driver`
+		CREATE TABLE user_idps (
+			idp BINARY(16),
+			sub STRING,
+			user BINARY(16),
+			PRIMARY KEY (idp, sub)
+			FOREIGN KEY (idp) REFERENCES idps(uid),
+			FOREIGN KEY (user) REFERENCES users(uid)
+		)
+	`;
+  }
+
+  async createUser(user: CreateUser): Promise<User> {
+    const uid = randomUUIDv7();
+    const [createdUser] = await this.driver`
+		INSERT INTO users (uid, username)
+		VALUES (${uid}, ${user.username})
+		RETURNING *
+	`;
+    return createdUser;
+  }
+
+  async findUser(idpIssuer: URL, idpSub: string): Promise<User | null> {
+    const response = await this.driver`
+		SELECT users.* FROM users, user_idps, idps
+		WHERE user_idps.sub = ${idpSub}
+		AND user_idps.idp = idps.uid
+		AND idps.issuer = ${idpIssuer.toString()}
+	`;
+    if (response.length === 0) {
+      return null;
+    }
+    if (response.length > 1) {
+      throw new Error("Duplicate idp user");
+    }
+    return response[0];
+  }
+
+  async createIdp(idp: CreateIdp): Promise<Idp> {
+    const uid = randomUUIDv7();
+    const [response] = await this.driver`
+		INSERT INTO idps (uid, issuer)
+		VALUES (${uid}, ${idp.issuer.toString()})
+		RETURNING *
+	`;
+    return {
+      ...response,
+      issuer: new URL(response.issuer),
+    };
+  }
+
+  async findIdp(issuer: URL): Promise<Idp | null> {
+    const response = await this.driver`
+		SELECT * from idps
+		WHERE idps.issuer = ${issuer.toString()}
+	`;
+    return response;
+  }
+
+  async linkUser(userId: string, idpId: string, idpSub: string): Promise<void> {
+    await this.driver`
+		INSERT INTO user_idps (idp, sub, user)
+		VALUES (${idpId}, ${idpSub}, ${userId})
+	`;
+  }
+}
+
+export { Database };
