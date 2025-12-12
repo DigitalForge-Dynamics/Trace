ddf0ca4c3f7c8192eb0a6ad3e8c2755c94b9a965
SolarFactories <SolarFactories@users.noreply.github.com>
2025-12-12 21:26:10 +0000

    [Local]: Add descriptions to Terraform variables used in local configuration. Updated docs.

--- docs/local-setup.md
+++ docs/local-setup.md
@@ -2,5 +2,11 @@
 
 1. Configure Keycloak Instance using `docker-compose`.
 1. Set values in `local/.env` file, to configure Terraform Provider to KeyCloak instance.
-1. Run `terraform apply`,to create a local development realm.
+1. Load the values from `local/.env` into current shell.
+	```sh
+	set -a
+	source local/.env
+	set +a
+	```
+1. Run `terraform apply`, to create a local development realm.
 1. Update values in `trace-api/.env`, `trace-playwright/.env`, `.env`.
--- local/variables.tf
+++ local/variables.tf
@@ -1,4 +1,17 @@
-variable "KC_ADMIN_URL" {}
-variable "KC_ADMIN_USERNAME" {}
-variable "KC_ADMIN_PASSWORD" {}
-variable "KC_ROOT_CA_PATH" {}
+variable "KC_ADMIN_URL" {
+  type        = string
+  description = "URL for the Keycloak instance / Realm. If no realm is provided, provider defaults to master."
+}
+variable "KC_ADMIN_USERNAME" {
+  type        = string
+  description = "Username to use to authenticate to Keycloak to configure new realm."
+}
+variable "KC_ADMIN_PASSWORD" {
+  type        = string
+  description = "Password to use to authenticate to Keycloak to configure new realm."
+  sensitive   = true
+}
+variable "KC_ROOT_CA_PATH" {
+  type        = string
+  description = "Optional path to Keycloak's certificate file for TLS."
+}
